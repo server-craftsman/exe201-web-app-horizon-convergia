373c058500f316fe8e6b5ffb2466aafe0974d06b
MinhQuandevfpt <quankontum55555@gmail.com>
2025-08-14 22:46:17 +0700

    payment_link_seller_product

--- src/layouts/seller/SellerHeader.tsx
+++ src/layouts/seller/SellerHeader.tsx
@@ -34,6 +34,15 @@ const SellerHeader: React.FC = () => {
             )
         },
         {
+            name: 'Đang Bán',
+            href: ROUTER_URL.SELLER.ACTIVE_PRODUCTS,
+            icon: (
+                <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            )
+        },
+        {
             name: 'Đơn Hàng',
             href: ROUTER_URL.SELLER.ORDERS,
             icon: (
---
+++ src/pages/seller/active-products/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ActiveProducts from '../../../components/seller/product/ActiveProducts.com';
+
+const ActiveProductsPage: React.FC = () => {
+    return <ActiveProducts />;
+};
+
+export default ActiveProductsPage;
